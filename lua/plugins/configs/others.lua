340afcc4f56a0728078757dd9f0235ce6d8c5732
lexuancuong <cuongle.software@gmail.com>
2022-04-28 16:02:22 +0700

    feat: add shortcuts for gitsigns

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -176,6 +176,18 @@ M.gitsigns = function(override_flag)
    local present, gitsigns = pcall(require, "gitsigns")
    if present then
       local default = {
+         keymaps = {
+            -- Default keymap options
+            buffer = true,
+            noremap = true,
+            ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
+            ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
+            ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+            ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+            ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+            ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+            ["n <leader>wc"] = '<cmd>lua require"gitsigns".blame_line()<CR>', -- wc mean who coded
+         },
          signs = {
             add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
             change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
