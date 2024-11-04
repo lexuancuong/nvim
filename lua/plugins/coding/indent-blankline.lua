9d75ffb26e83962ac598e98e26eaea113f0932e0
lexuancuong <cuong.le@parcelperform.com>
2024-11-04 10:41:05 +0700

    Remove redundant configurations for indent-blankline

--- lua/plugins/coding/indent-blankline.lua
+++ lua/plugins/coding/indent-blankline.lua
@@ -1,27 +1,5 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  opts = {
-    char = "▏",
-    use_treesitter = true,
-    show_first_indent_level = true,
-    filetype_exclude = {
-      "lspinfo",
-      "packer",
-      "checkhealth",
-      "help",
-      "man",
-      "dashboard",
-      "git",
-      "markdown",
-      "text",
-      "terminal",
-      "NvimTree",
-    },
-    buftype_exclude = {
-      "terminal",
-      "nofile",
-      "quickfix",
-      "prompt",
-    },
-  },
+  main = "ibl",
+  opts = {},
 }
