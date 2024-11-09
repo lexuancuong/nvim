40e11497bb79718fc9d6bebb1b6df4232ccd9fc2
lexuancuong <cuong.le@parcelperform.com>
2024-11-09 21:58:34 +0700

    :hammer: Update avante.nvim configuration

--- lua/plugins/finder/nvim-spectre.lua
+++ lua/plugins/finder/nvim-spectre.lua
@@ -1,6 +1,7 @@
 return {
   "windwp/nvim-spectre",
   lazy = true,
+  enable = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     -- 'kyazdani42/nvim-web-devicons',
--- lua/plugins/lsp/avante.lua
+++ lua/plugins/lsp/avante.lua
@@ -3,6 +3,40 @@ return {
   event = "VeryLazy",
   version = false,
   build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
   opts = {
     provider = "claude",
     auto_suggestions_provider = "claude",
@@ -12,19 +46,21 @@ return {
     },
     ui = {
       position = "right",
-      width = 30,
+      width = 35,
       border = "rounded",
     },
     behaviour = {
-      auto_suggestions = false,
+      auto_suggestions = true,
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
+      suggestion_context_size = 3000,
     },
     keymaps = {
       toggle = "<leader>aa",
       refresh = "<leader>ar",
       focus = "<leader>af",
       edit = "<leader>ae",
+      load_project_context = "<leader>ap",
     },
   },
   config = function(_, opts)
@@ -39,6 +75,7 @@ return {
       print("Failed to setup Avante:", err)
       return
     end
+    vim.api.nvim_set_keymap("n", "<leader>ap", ":lua require'avante'.load_context()<CR>", { noremap = true, silent = true })
   end,
 }
 -- Create dir manually for full writing on it
