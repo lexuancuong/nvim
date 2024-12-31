55bcd2e44ac869206bfa7259a9817829a1440570
lexuancuong <cuong.le@parcelperform.com>
2024-12-31 23:52:14 +0700

    Add mason configs

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -24,17 +24,17 @@ return {
       -- list of servers for mason to install
       ensure_installed = {
         "pyright",
+        "tsserver", -- typescript/javascript server
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
     })
 
     mason_tool_installer.setup({
-      ensure_installed = {
-        "isort", -- python formatter
-        "black", -- python formatter
-        -- "pylint", -- python linter
-      },
+      "isort", -- python formatter
+      "black", -- python formatter
+      "prettier", -- javascript/typescript formatter
+      "eslint_d", -- javascript linter
     })
   end,
 }
