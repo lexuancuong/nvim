9a2320c580893fcd2055059a52fa0b2c837992a7
lexuancuong <cuong.le@parcelperform.com>
2025-06-02 19:44:38 +0700

    Add enter keymap for nvim cmp

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -80,6 +80,7 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
